069d029e794b1b8ad9baf98f90a496cdb0083bde
chandangowa <chandangowdacb@gmail.com>
2019-03-25 07:57:43 +0530

    first commit

---
+++ Oshop-AuthServer/src/main/java/com/oshop/OshopAuthServer/OshopAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.oshop.OshopAuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshopAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshopAuthServerApplication.class, args);
+	}
+
+}
---
+++ Oshop-gateway/src/main/java/com/oshopgateway/OshopGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.oshopgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableWebSecurity
+public class OshopGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshopGatewayApplication.class, args);
+	}
+
+}
