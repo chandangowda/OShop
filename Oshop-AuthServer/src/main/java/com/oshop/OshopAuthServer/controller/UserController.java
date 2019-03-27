826bf9c32b60e68268fbd8f51cde2223d139186a
chandangowa <chandangowdacb@gmail.com>
2019-03-27 22:40:07 +0530

    Java Changes

---
+++ Oshop-AuthServer/src/main/java/com/oshop/OshopAuthServer/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.oshop.OshopAuthServer.controller;
+
+import java.security.Principal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@GetMapping("/authenticate")
+	  public ResponseEntity<Principal> user(Principal user) {
+	   return ResponseEntity.<Principal>ok(user);
+	  }
+
+}
