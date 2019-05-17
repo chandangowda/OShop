279f9dad9170a4a4526f876c87a7d7f94be7b60d
Chandangowda <chandangowda.chikkabasavanahallibaburam@centurylink.com>
2019-05-17 19:01:46 +0530

    Cart count

---
+++ Oshop-Product/Oshop-Product/src/main/java/com/oshop/oshopproduct/dto/ItemDto.java
@@ -0,0 +1,20 @@
+package com.oshop.oshopproduct.dto;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class ItemDto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7161062202983802393L;
+
+	private String productId;
+
+	private int cartCount;
+
+}
