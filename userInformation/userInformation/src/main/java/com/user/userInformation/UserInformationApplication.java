9bcbaee6a6aaae6115fbd8467999343d77c99838
Chandangowda <chandangowda.chikkabasavanahallibaburam@centurylink.com>
2019-06-03 16:45:45 +0530

    User creation

--- userInformation/userInformation/src/main/java/com/user/userInformation/UserInformationApplication.java
+++ userInformation/userInformation/src/main/java/com/user/userInformation/UserInformationApplication.java
@@ -5,21 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @SpringBootApplication
 @EnableResourceServer
 @EnableEurekaClient
-public class UserInformationApplication {
+public class UserInformationApplication extends ResourceServerConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserInformationApplication.class, args);
 	}
-	
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) {
+		resources.resourceId("UserInfo");
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.anonymous().and().authorizeRequests().antMatchers("/user/saveuser").permitAll();
+	}
+
 	@Bean
 	public DozerBeanMapper dozerBean() {
-		DozerBeanMapper dozerBean = new DozerBeanMapper();
-		return dozerBean;
+		return   new DozerBeanMapper();
 	}
 
 }
--- userInformation/userInformation/src/main/java/com/user/userInformation/entity/Role.java
+++ userInformation/userInformation/src/main/java/com/user/userInformation/entity/Role.java
@@ -6,7 +6,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.IndexDirection;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection="roles")
 public class Role implements Serializable {
 
 	/**
